ee42db3c62a83fc54f4448f660d64bf51ed1ad79
Diego Resendiz Ojeda <di_564@hotmail.com>
2021-01-10 19:44:36 -0800

    cambios

---
+++ index.js
@@ -0,0 +1,63 @@
+let express = require('express');
+let app = express();
+let pieRepo = require('./repos/pieRepo')
+
+let router = express.Router();
+
+
+router.get('/', function (req, res, next) {
+    pieRepo.get(function (data) {
+
+        res.status(200).json({
+            "status": 200,
+            "statusText": "OK",
+            "message:": "All pies retrieved",
+            "data": data
+        });
+    });
+
+});
+
+router.get('/:id', function (req, res, next) {
+    pieRepo.getById(req.params.id, function (data) {
+
+            if (data) {
+                res.status(200).json({
+                    "status": 200,
+                    "statusText": "OK",
+                    "message:": "All pies retrieved",
+                    "data": data
+                });
+            } else {
+                res.status(404).json({
+                    "status": 404,
+                    "statusText": "Not Found",
+                    "message:": `the pie  ${req.params.id} could not found`,
+                    "error": {
+                        "code": "NOT_FOUND",
+                        "message:": `the pie  ${req.params.id} could not found`
+                    }
+                });
+            }
+        },
+        function (err) {
+            next(err);
+        }
+    )
+    ;
+
+});
+
+app.use('/api/', router);
+
+var server = app.listen(5000, function () {
+
+    console.log('Node server is running');
+
+});
+
+
+
+
+
+
